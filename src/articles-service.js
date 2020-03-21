1116be9933c5e6c48763cab9f34e63f713c6967f
Kevin Park <keunsuk413@gmail.com>
2020-03-21 15:28:22 -0500

    complete test for getting queries using test db

--- src/articles-service.js
+++ src/articles-service.js
@@ -1,7 +1,36 @@
 const ArticlesService = {
-  getAllArticles() {
-    return 'all the articles!!'
-  }
-}
+  getAllArticles(knex) {
+    // return 'all the articles!!'
+    // return Promise.resolve('all the articles!!')
+    return knex.select('*').from('blogful_articles');
+  },
 
-module.exports = ArticlesService
+  insertArticle(knex, newArticle) {
+    return knex
+      .insert(newArticle)
+      .into('blogful_articles')
+      .returning('*')
+      .then(rows => {
+        return rows[0];
+      });
+  },
+  getById(knex, id) {
+    return knex
+      .from('blogful_articles')
+      .select('*')
+      .where('id', id)
+      .first();
+  },
+  deleteArticle(knex, id) {
+    return knex('blogful_articles')
+      .where({ id })
+      .delete();
+  },
+  updateArticle(knex, id, newArticleFields) {
+    return knex('blogful_articles')
+      .where({ id })
+      .update(newArticleFields);
+  }
+};
+
+module.exports = ArticlesService;
